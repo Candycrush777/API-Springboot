685b03042c03af4325fd257bab3a94fd5d1310d6
Candycrush777 <eduardoafan.eae@gmail.com>
2025-09-04 19:25:15 +0200

    archivos inicales

---
+++ src/test/java/com/agenda/agenda_hexagonal/AgendaHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda.agenda_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
